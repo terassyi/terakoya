fce20e54e07525639f3c4fe490fb5f0e2290499e
terassyi <iscale821@gmail.com>
2025-05-23 22:13:03 +0900

    WIP: nix
    Signed-off-by: terassyi <iscale821@gmail.com>

---
+++ nix/src/home/linux/teradev/tools/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }: {
+  home.packages =
+    with pkgs; [ # Desktop user interface for managing virtual machines
+      virt-manager
+      # Toolkit to interact with the virtualization capabilities of recent versions of Linux and other OSes
+      libvrit
+      # Viewer for remote virtual machines
+      virt-viewer
+    ];
+}
